7f63fb95af3e3aadc6fb61ad94cd2eace4b89dd6
farrukh90 <farrukhsadykov@gmail.com>
2020-11-07 21:34:44 +0000

    added policy and role

---
+++ class1/policy.tf
@@ -0,0 +1,18 @@
+resource "aws_iam_policy" "policy" {
+  name = "test_policy3"
+  path = "/"
+  description = "My test policy"
+  policy = <<EOF
+{
+"Version": "2012-10-17",
+"Statement": [
+{
+"Sid": "VisualEditor0",
+"Effect": "Allow",
+"Action": "ec2:*",
+"Resource": "*"
+}
+]
+}
+EOF
+}
