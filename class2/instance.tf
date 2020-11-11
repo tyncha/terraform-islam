82f5e897ed7d9e7fd10c0a3717b3b57ad04280c8
farrukh90 <farrukhsadykov@gmail.com>
2020-11-11 00:47:35 +0000

    added a new rule

--- class2/instance.tf
+++ class2/instance.tf
@@ -3,4 +3,6 @@ resource "aws_instance" "web" {
 	instance_type = "t2.micro"
     associate_public_ip_address = "true"
     key_name = "${aws_key_pair.class.key_name}"
+    user_data = "${file("userdata_file.sh")}"
+    vpc_security_group_ids = ["${aws_security_group.class2.id}"]
 }
