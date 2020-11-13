0e3afd356342a78485f065b4c3521a6095512109
farrukh90 <farrukhsadykov@gmail.com>
2020-11-13 00:32:51 +0000

    added ami data

---
+++ class3/instance.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "ubuntu" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t3.micro"
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+
+resource "aws_instance" "centos" {
+  ami           = "${data.aws_ami.centos.id}"
+  instance_type = "t3.micro"
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
