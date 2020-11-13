d7d59939b2955d0ca362a7fcda07053e02cc819b
farrukh90 <farrukhsadykov@gmail.com>
2020-11-13 00:45:47 +0000

    added az data

--- class3/instance.tf
+++ class3/instance.tf
@@ -1,12 +1,11 @@
 resource "aws_instance" "ubuntu" {
   ami           = "${data.aws_ami.ubuntu.id}"
+  availability_zone = "${data.aws_availability_zones.available.names[3]}"
   instance_type = "t3.micro"
   tags = {
     Name = "HelloWorld"
   }
 }
-
-
 resource "aws_instance" "centos" {
   ami           = "${data.aws_ami.centos.id}"
   instance_type = "t3.micro"
