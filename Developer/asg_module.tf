a8e076c2f3ec95b189fcf9bd4883ebf4defc34b0
farrukh90 <farrukhsadykov@gmail.com>
2020-11-14 21:36:51 +0000

    added  variable

--- Developer/asg_module.tf
+++ Developer/asg_module.tf
@@ -1,8 +1,16 @@
 module "webapp" {
     source = "../class4"
+    region = "us-east-1"
+    max_size = "1"
+    min_size = "1"
+    desired_capacity = "1"
 }
 
 
 module "python" {
     source = "../class4"
+    region = "us-east-1"
+    max_size = "1"
+    min_size = "1"
+    desired_capacity = "1"
 }
