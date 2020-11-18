1884e4d6e2a09d255d1fe04a0343f3e02ffbf81e
farrukh90 <farrukhsadykov@gmail.com>
2020-11-18 01:37:49 +0000

    added readme

--- Developer/asg_module.tf
+++ Developer/asg_module.tf
@@ -5,31 +5,5 @@ module "webapp" {
   min_size         = "1"
   desired_capacity = "1"
   image_owner      = "137112412989"
+  instance_type    = "c5.large"
 }
-
-# module "python" {
-#   source           = "../class4"
-#   region           = "us-west-2"
-#   max_size         = "1"
-#   min_size         = "1"
-#   desired_capacity = "1"
-#   image_owner      = "137112412989"
-# }
-
-# module "python-eu" {
-#   source           = "../class4"
-#   region           = "eu-west-1"
-#   max_size         = "1"
-#   min_size         = "1"
-#   desired_capacity = "1"
-#   image_owner      = "137112412989"
-# }
-
-# module "python-ap" {
-#   source           = "../class4"
-#   region           = "ap-southeast-1"
-#   max_size         = "1"
-#   min_size         = "1"
-#   desired_capacity = "1"
-#   image_owner      = "137112412989"
-# }
