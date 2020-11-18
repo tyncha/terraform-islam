18455ab30797ad3a3c8189a3146d229760facb75
farrukh90 <farrukhsadykov@gmail.com>
2020-11-18 02:14:49 +0000

    added provider

--- class5/vpc.tf
+++ class5/vpc.tf
@@ -1,3 +1,19 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
+
+resource "aws_subnet" "subnet1" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.1.0/24"
+}
+
+
+resource "aws_subnet" "subnet2" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.2.0/24"
+}
+
+resource "aws_subnet" "subnet3" {
+  vpc_id     = "${aws_vpc.main.id}"
+  cidr_block = "10.0.3.0/24"
+}
