86b0764bbddebc0d3ff31f9c45327d9aa66ad789
farrukh90 <farrukhsadykov@gmail.com>
2020-11-18 00:15:48 +0000

    added elb

--- Developer/asg_module.tf
+++ Developer/asg_module.tf
@@ -7,29 +7,29 @@ module "webapp" {
   image_owner      = "137112412989"
 }
 
-module "python" {
-  source           = "../class4"
-  region           = "us-west-2"
-  max_size         = "1"
-  min_size         = "1"
-  desired_capacity = "1"
-  image_owner      = "137112412989"
-}
+# module "python" {
+#   source           = "../class4"
+#   region           = "us-west-2"
+#   max_size         = "1"
+#   min_size         = "1"
+#   desired_capacity = "1"
+#   image_owner      = "137112412989"
+# }
 
-module "python-eu" {
-  source           = "../class4"
-  region           = "eu-west-1"
-  max_size         = "1"
-  min_size         = "1"
-  desired_capacity = "1"
-  image_owner      = "137112412989"
-}
+# module "python-eu" {
+#   source           = "../class4"
+#   region           = "eu-west-1"
+#   max_size         = "1"
+#   min_size         = "1"
+#   desired_capacity = "1"
+#   image_owner      = "137112412989"
+# }
 
-module "python-ap" {
-  source           = "../class4"
-  region           = "ap-southeast-1"
-  max_size         = "1"
-  min_size         = "1"
-  desired_capacity = "1"
-  image_owner      = "137112412989"
-}
+# module "python-ap" {
+#   source           = "../class4"
+#   region           = "ap-southeast-1"
+#   max_size         = "1"
+#   min_size         = "1"
+#   desired_capacity = "1"
+#   image_owner      = "137112412989"
+# }
